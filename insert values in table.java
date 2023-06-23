10781316715a49f32bd2cdb89ff107e5115ded91
Anirban <134738334+Anirban-Gh0sh@users.noreply.github.com>
2023-06-24 02:19:36 +0530

    Create insert values in table.java
    using create statement

---
+++ insert values in table.java
@@ -0,0 +1,39 @@
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.Statement;
+public class JDBC
+{
+    /*Steps to connect with DB and perform operations 
+       1.Load driver
+       2.create connection
+       3.create statement
+       4.execute query
+       5.close connection*/
+    public void insertValueInTable()
+    {
+        //using simple statement not prepared statement
+        String url="jdbc:mysql://localhost:3306/STUDENT";
+        String userName="root";
+        String password="Password";
+        String query="INSERT INTO CSE(REG_NO,NAME,PHONE_NUMBER,GENDER,CITY) VALUES(2,'Anil Kumar',8053688545,'M','Bhubaneswar')";
+        
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con=DriverManager.getConnection(url,userName,password);
+            Statement stmt=con.createStatement();
+            stmt.executeUpdate(query);
+            System.out.println("Successful");
+            con.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+    public static void main(String args[])
+    {
+        JDBC jd=new JDBC();
+        jd.insertValueInTable();
+    }
+}
